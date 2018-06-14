a77b7f2e245c7b05f8bf98e647786c6329d2a235
thomaspj10 <thomaspj2003@gmail.com>
2018-06-14 18:29:46 +0200

    Custom Item API

---
+++ src/ttdev/API.java
@@ -0,0 +1,19 @@
+package ttdev;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class API extends JavaPlugin {
+
+	private static API singleton;
+	
+	public static API getInstance() {
+		return singleton;
+	}
+	
+	public void onEnable() {
+		singleton = this;
+		
+		
+	}
+	
+}
---
+++ src/ttdev/items/Item.java
@@ -0,0 +1,54 @@
+package ttdev.items;
+
+
+import java.util.List;
+import java.util.Map;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class Item {
+
+	private ItemStack itemStack;
+	private ItemMeta itemMeta;
+	
+	public Item(ItemStack itemStack) {
+		this.itemStack = itemStack;
+		this.itemMeta = this.itemStack.getItemMeta();
+	}
+	
+	public ItemStack getItemStack() {
+		this.itemStack.setItemMeta(itemMeta);
+		return this.itemStack;
+	}
+	
+	public void addEnchant(Enchantment enchantment, int level) {
+		this.itemMeta.addEnchant(enchantment, level, false);
+	}
+	
+	public void removeEnchant(Enchantment enchantment) {
+		this.itemMeta.removeEnchant(enchantment);
+	}
+	
+	public void setName(String name) {
+		this.itemMeta.setDisplayName(name);
+	}
+	
+	public void setLore(List<String> lore) {
+		this.itemMeta.setLore(lore);
+	}
+	
+	public String getName() {
+		return this.itemMeta.getDisplayName();
+	}
+	
+	public Map<Enchantment, Integer> getEnchants() {
+		return this.itemMeta.getEnchants();
+	}
+	
+	public List<String> getLore() {
+		return this.itemMeta.getLore();
+	}
+	
+}
