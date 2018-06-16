38ec7ef5446496b7ad3cd0390ca8bd2957bf79ae
BuildTools <unconfigured@null.spigotmc.org>
2018-06-15 23:30:54 -0400

    Custom redstone event handling

--- src/ttdev/api/redstone/event/RedstoneTriggerEvent.java
+++ src/ttdev/api/redstone/event/RedstoneTriggerEvent.java
@@ -1,7 +1,51 @@
 package ttdev.api.redstone.event;
 
-public class RedstoneTriggerEvent {
+import org.bukkit.block.Block;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.material.RedstoneWire;
+import ttdev.api.redstone.TriggerType;
 
+import java.util.List;
 
+public class RedstoneTriggerEvent extends Event
+{
 
+    private static HandlerList handlerList = new HandlerList();
+
+    private Block callingBlock;
+    private List<RedstoneWire> redstoneWires;
+    private TriggerType triggerType;
+
+    public RedstoneTriggerEvent(Block callingBlock, List<RedstoneWire> redstoneWires, TriggerType triggerType)
+    {
+        this.callingBlock = callingBlock;
+        this.redstoneWires = redstoneWires;
+        this.triggerType = triggerType;
+    }
+
+    public Block getCallingBlock()
+    {
+        return callingBlock;
+    }
+
+    public List<RedstoneWire> getRedstoneWires()
+    {
+        return redstoneWires;
+    }
+
+    public TriggerType getTriggerType()
+    {
+        return triggerType;
+    }
+
+    public HandlerList getHandlers()
+    {
+        return handlerList;
+    }
+
+    public static HandlerList getHandlerList()
+    {
+        return handlerList;
+    }
 }
