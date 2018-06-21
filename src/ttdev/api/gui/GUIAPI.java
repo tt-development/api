14d0a9fecf7c97ed2233c759d3c9f012cb5f00ff
TTGTRE <tr3.logan@gmail.com>
2018-06-20 22:30:34 -0400

    Remove duplicate JavaPlugin

--- src/ttdev/api/gui/GUIAPI.java
+++ src/ttdev/api/gui/GUIAPI.java
@@ -2,7 +2,7 @@ package ttdev.api.gui;
 
 
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
+import ttdev.api.API;
 import ttdev.api.gui.util.PlaceholderManager;
 
 import java.util.HashMap;
@@ -12,23 +12,10 @@ import java.util.Map;
  *
  * @author Tre Logan
  */
-public class GUIAPI extends JavaPlugin {
-    
-    private static JavaPlugin plugin;
+public class GUIAPI {
     
     private static Map<String, Listener> menuListeners = new HashMap<>();
     private static PlaceholderManager placeholderManager = new PlaceholderManager();
-    
-    @Override
-    public void onEnable() {
-        plugin = this;
-        getLogger().info(getName() + " enabled.");
-    }
-    
-    @Override
-    public void onDisable() {
-        getLogger().info(getName() + " disabled.");
-    }
     
     public static void addMenuListener(String id, Listener listener) {
         Listener previous = menuListeners.put(id, listener);
@@ -39,7 +26,8 @@ public class GUIAPI extends JavaPlugin {
     }
     
     private static void registerListener(Listener listener) {
-        plugin.getServer().getPluginManager().registerEvents(listener, plugin);
+        API apiInstance = API.getInstance();
+        apiInstance.getServer().getPluginManager().registerEvents(listener, apiInstance);
     }
 
     public static PlaceholderManager getPlaceholderManager() {
