b11430caaf0ac9703a4b99b2fa9691f4ccba4d00
TTGTRE <tr3.logan@gmail.com>
2018-09-24 12:10:59 -0400

    move FillColor into Fill.kt and remove unnecessary methods

--- src/ttdev/api/user/inventory/fill/Fill.kt
+++ src/ttdev/api/user/inventory/fill/Fill.kt
@@ -22,3 +22,28 @@ data class Fill(val one: FillColor?, val two: FillColor?, val three: FillColor?,
     }
 
 }
+
+enum class FillColor(val id: Short) {
+
+    WHITE(0),
+    ORANGE(1),
+    MAGENTA(2),
+    LIGHT_BLUE(3),
+    YELLOW(4),
+    LIME(5),
+    PINK(6),
+    GRAY(7),
+    LIGHT_GRAY(8),
+    CYAN(9),
+    PURPLE(10),
+    BLUE(11),
+    BROWN(12),
+    GREEN(13),
+    RED(14),
+    BLACK(15);
+
+    fun getFillColor(id: Short): FillColor? {
+        return values().find { id == it.id }
+    }
+
+}
